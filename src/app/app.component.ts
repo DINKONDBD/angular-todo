b7d8ecac56bf3430281e514c290df503e8ed7e3a
DINKONDBD <simas2032@gmail.com>
2022-08-04 21:36:05 +0300

    a m o g u s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit {
+
+  public TODO_ITEMS:string[] = [];
+  constructor() { }
+
+  ngOnInit(): void {}
+  public addtodos(txt:string)
+  {
+    this.TODO_ITEMS.push(txt);
+
+  }
+  public removetodo(a:any)
+  {
+    this.TODO_ITEMS.splice(this.TODO_ITEMS.indexOf(a), 1)
+    console.log(this.TODO_ITEMS.indexOf(a))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// angular material
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
